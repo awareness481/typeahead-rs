6dc4ea06e87bc588b828dd15e85b3887288b5128
Jesse Jafa <jesse.jafa@gmail.com>
2021-11-03 12:39:33 +0200

    initial search through cities :)

---
+++ src/json.rs
@@ -0,0 +1,21 @@
+use serde_json::{Value};
+use std::fs;
+
+pub fn read_json_file(filename: String) -> Vec<String> {
+    // --snip--
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    let v: Value = serde_json::from_str(&contents).unwrap();
+
+    // Access parts of the data by indexing with square brackets.
+    let mut cities: Vec<String> = Vec::new();
+
+    for city in v.as_array().unwrap().iter() {
+        cities.push(city["name"].as_str().unwrap().to_lowercase());
+    }
+
+    cities
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use sequence_trie::SequenceTrie;
+mod json;
 
 fn main() {
+    let cities = json::read_json_file(String::from("cities.json"));
+
     let mut trie: SequenceTrie<char, String> = SequenceTrie::new();
     trie.insert(&['h', 'e', 'e'], String::from("hbe"));
     trie.insert(&['h', 'e', 'l', 'l', 'o'], String::from("hello"));
+
+    for city in cities.iter() {
+        trie.insert(&city.chars().collect::<Vec<char>>(), String::from(city));
+    }
+    dbg!(cities);
+
     // println!(
     //     "{:?}",
     //     trie.get_node(&['h', 'e'])
@@ -14,10 +23,7 @@ fn main() {
     //         .into_iter()
     //         .collect::<String>()
     // );
-    let results = trie
-        .get_node(&['h', 'e'])
-        .unwrap()
-        .values();
+    let results = trie.get_node(&['a', 't']).unwrap().values();
 
     results.into_iter().for_each(|x| println!("{}", x));
 }
