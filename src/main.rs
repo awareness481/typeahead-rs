0190d3969eb06fdc371fb572aaff2136c617b50b
Jesse Jafa <jesse.jafa@gmail.com>
2021-11-03 11:24:43 +0200

    initial impl

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,23 @@
 use sequence_trie::SequenceTrie;
 
 fn main() {
-    let mut trie: SequenceTrie<char, i32> = SequenceTrie::new();
-    trie.insert(&['h', 'b', 'e'], 3);
-    trie.insert(&['h', 'e', 'l', 'l', 'o'], 5);
-    print!(
-        "{:?}",
-        trie.get_node(&['h', 'e'])
-            .unwrap()
-            .keys()
-            .flatten()
-            .collect::<Vec<&char>>()
-            .into_iter()
-            .collect::<String>()
-    );
+    let mut trie: SequenceTrie<char, String> = SequenceTrie::new();
+    trie.insert(&['h', 'e', 'e'], String::from("hbe"));
+    trie.insert(&['h', 'e', 'l', 'l', 'o'], String::from("hello"));
+    // println!(
+    //     "{:?}",
+    //     trie.get_node(&['h', 'e'])
+    //         .unwrap()
+    //         .keys()
+    //         .flatten()
+    //         .collect::<Vec<&char>>()
+    //         .into_iter()
+    //         .collect::<String>()
+    // );
+    let results = trie
+        .get_node(&['h', 'e'])
+        .unwrap()
+        .values();
+
+    results.into_iter().for_each(|x| println!("{}", x));
 }
