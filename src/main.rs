a8eb8e069977476d76a70f1dab81cbb1279f8ffa
Jesse Jafa <jesse.jafa@gmail.com>
2021-11-04 08:10:49 +0200

    initial terminal input

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use sequence_trie::SequenceTrie;
+use std::io::stdin;
 mod json;
 
 fn main() {
@@ -8,8 +9,25 @@ fn main() {
     for city in cities.iter() {
         trie.insert(&city.chars().collect::<Vec<char>>(), String::from(city));
     }
-    
-    let results = trie.get_node(&['a', 't']).unwrap().values();
+
+    println!("Input: ");
+    let mut input = get_input().to_lowercase();
+    input.pop();
+
+    let results = trie.get_node(&input.chars().collect::<Vec<char>>()).unwrap().values();
 
     results.into_iter().for_each(|x| println!("{}", x));
+}
+
+fn get_input() -> String {
+    let mut input = String::new();
+    match stdin().read_line(&mut input) {
+        Ok(n) => {
+            println!("{} bytes read", n);
+            println!("{}", input);
+        }
+        Err(error) => println!("error: {}", error),
+    }
+
+    input
 }
